35d6355bfee426b762569499c0236aabf41f55b6
Agustin <agustinmmorales0709@gmail.com>
2023-05-29 19:47:42 -0300

    trabajo finalizado

--- morales-ibarra-villa-zelada-givogri/src/Componentes/ListaMiembros.js
+++ morales-ibarra-villa-zelada-givogri/src/Componentes/ListaMiembros.js
@@ -4,13 +4,14 @@ function ListaMiembros() {
 
   const listaMiembros = [
     {id:1, miembro:'Agustin Morales', pertenece: true},
-    {id:1, miembro:'Daniela Villa', pertenece: true},
-    {id:1, miembro:'Juan Roman Riquelme', pertenece: false},
-    {id:1, miembro:'Santiago Zelada', pertenece: true},
-    {id:1, miembro:'Alejandro Sanz', pertenece: false},
-    {id:1, miembro:'Lucia Natalicio', pertenece: false},
-    {id:1, miembro:'Ignacio Ibarra', pertenece: true},
-    {id:1, miembro:'Agustin Givogri', pertenece: true},
+    {id:2, miembro:'Daniela Villa', pertenece: true},
+    {id:3, miembro:'Juan Roman Riquelme', pertenece: false},
+    {id:4, miembro:'Santiago Zelada', pertenece: true},
+    {id:5, miembro:'Alejandro Garnacho', pertenece: false},
+    {id:6, miembro:'Lucia Natalicio', pertenece: false},
+    {id:7, miembro:'Ignacio Ibarra', pertenece: true},
+    {id:8, miembro:'Agustin Givogri', pertenece: true},
+    {id:9, miembro:'Lionel Andres MESSI Cuccitini', pertenece: true},
   ]
   return (
     <>
--- morales-ibarra-villa-zelada-givogri/src/Componentes/SliderImg.jsx
+++ morales-ibarra-villa-zelada-givogri/src/Componentes/SliderImg.jsx
@@ -8,7 +8,7 @@ function SliderImg() {
                 <Col sm={6} md={8}>
                 <Carousel>
                     <Carousel.Item>
-                        <img className="d-block w-100" src="https://scontent.ftuc1-2.fna.fbcdn.net/v/t39.30808-6/305624480_457260179759639_8308490097705665910_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGq905DzajilV0cSGMCm2EM7Q0-b8TkkUvtDT5vxOSRS6Ekl9LB86LWvlOnHe_2ZUk&_nc_ohc=R38ODt8zB6wAX8VnWP9&_nc_ht=scontent.ftuc1-2.fna&oh=00_AfD-sTXY1N4F5lc4Z1HmgntCT1StWdSZUxUQgBN8HCVGsw&oe=647625C8" alt="First Img Slider" fluid />
+                        <img className="d-block w-100" src="https://images-ext-1.discordapp.net/external/1bgar9zK5ysCr8RtyauUIjSJPw_XqH2gy0ADeOjmJPY/%3F_nc_cat%3D109%26ccb%3D1-7%26_nc_sid%3De3f864%26_nc_eui2%3DAeGq905DzajilV0cSGMCm2EM7Q0-b8TkkUvtDT5vxOSRS6Ekl9LB86LWvlOnHe_2ZUk%26_nc_ohc%3DR38ODt8zB6wAX8VnWP9%26_nc_ht%3Dscontent.ftuc1-2.fna%26oh%3D00_AfD-sTXY1N4F5lc4Z1HmgntCT1StWdSZUxUQgBN8HCVGsw%26oe%3D647625C8/https/scontent.ftuc1-2.fna.fbcdn.net/v/t39.30808-6/305624480_457260179759639_8308490097705665910_n.jpg?width=750&height=468" alt="First Img Slider" fluid />
                         <Carousel.Caption>
                             <h3>Bienvenidos</h3>
                         </Carousel.Caption>
