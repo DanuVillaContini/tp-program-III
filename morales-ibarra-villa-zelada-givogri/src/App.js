87625fa8740f89f96bea9ce94fa5e6bcc80fade3
Danu Villa <126210362+DanuVillaContini@users.noreply.github.com>
2023-06-26 23:45:46 -0300

    ultimo committ con solucion de enrutamiento netlify

--- morales-ibarra-villa-zelada-givogri/src/App.js
+++ morales-ibarra-villa-zelada-givogri/src/App.js
@@ -1,23 +1,23 @@
-import'./Componentes/GridLayout';
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./screen/Home";
-import Shop from "./screen/Shop";
-import AboutUs from "./screen/AboutUs";
-import Register from "./screen/Register";
-import Login from "./screen/Login";
-import Error from "./screen/Error";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './screen/Home';
+import Shop from './screen/Shop';
+import AboutUs from './screen/AboutUs';
+import Register from './screen/Register';
+import Login from './screen/Login';
+import Error from './screen/Error';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/Shop" element={<Shop/>}></Route>
-          <Route path="/aboutUs" element={<AboutUs/>}></Route>
-          <Route path="/register" element={<Register/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/error" element={<Error/>}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/Shop" element={<Shop />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/error" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </>
